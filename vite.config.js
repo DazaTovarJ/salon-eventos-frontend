a7e0a83c28b93a34f072625d3ed0a28b8d3abe78
Jaime David Daza Tovar <jaimedaviddazat2001@gmail.com>
2026-05-05 02:29:24 +0000

    test(vite): :white_check_mark: add Vitest

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
+/// <reference types="vitest/config" />
+
 import {defineConfig} from "vite";
-import react from "@vitejs/plugin-react";
 import eslint from "vite-plugin-eslint";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig(() => {
   return {
@@ -8,5 +10,10 @@ export default defineConfig(() => {
       outDir: "build",
     },
     plugins: [react(), eslint()],
+    test: {
+      globals: true,
+      environment: "jsdom",
+      setupFiles: "./tests/setup.js",
+    },
   };
 });
